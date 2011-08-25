f0762827fa7a4c57bdd2e50b66fc795442c3daa9
Emerson Tiago <emerson.etns@gmail.com>
2011-08-25 10:14:14 -0300

    Added urls to serve static files

--- urls.py
+++ urls.py
@@ -8,4 +8,5 @@ urlpatterns = patterns('',
     # url(r'^minicursos_tecnologos/', include('minicursos_tecnologos.foo.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^site_media/(.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 )
